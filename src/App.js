0b8684d94a90ca2701b568dd9d4a595db0c5e4e7
Adrian Matei <ama@codepedia.org>
2019-08-19 17:37:53 +0200

    display latest public dev bookmarks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            const result = await axios(
+                'https://www.bookmarks.dev/api/public/bookmarks',
+            );
+            setData(result.data);
+        };
+
+        fetchData();
+    }, []);
+
+
+    return (
+
+        <div>
+            <h1>Latest Public Bookmarks</h1>
+            {data.map((bookmark) => (
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{bookmark.name}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{bookmark.location}</h6>
+                        <p className="card-text">{bookmark.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
